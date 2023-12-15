a69ca2c2a69e71a8a2454d3d407fcad12e6c1773
awaghadpd <74618130+awaghadpd@users.noreply.github.com>
2023-12-15 10:46:26 +0530

    Update configure-ingress-controller.sh

--- testportal/configure-ingress-controller.sh
+++ testportal/configure-ingress-controller.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 NAMESPACE=$1
-DEPLOYMENT=$2
-INGRESS=$3
-SERVICE=$4
+INGRESS=$2
+
 
 
 # create namespace if doesn't exists
 kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
-
-# Create aks-helloworld and ingress-demo
-kubectl apply -f $DEPLOYMENT --namespace $NAMESPACE
 kubectl apply -f $INGRESS --namespace $NAMESPACE
-kubectl apply -f $SERVICE --namespace $NAMESPACE
+kubectl apply -f deployment.yaml --namespace $NAMESPACE
+kubectl apply -f service.yaml --namespace $NAMESPACE
+
