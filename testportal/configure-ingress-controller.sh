7c2d554db63ab89977c2f9479510127ce66f4f93
awaghadpd <74618130+awaghadpd@users.noreply.github.com>
2023-12-15 11:17:42 +0530

    Update configure-ingress-controller.sh

--- testportal/configure-ingress-controller.sh
+++ testportal/configure-ingress-controller.sh
@@ -7,6 +7,6 @@ INGRESS=$2
 # create namespace if doesn't exists
 kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 kubectl apply -f $INGRESS --namespace $NAMESPACE
-kubectl apply -f deployment.yaml --namespace $NAMESPACE
-kubectl apply -f service.yaml --namespace $NAMESPACE
+# kubectl apply -f deployment.yaml --namespace $NAMESPACE
+# kubectl apply -f service.yaml --namespace $NAMESPACE
 
